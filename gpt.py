c2406656c06f58df665bf70b6476ef4f19e7eeb2
durganeha <165761657+durganeha@users.noreply.github.com>
2025-01-26 12:41:10 +0530

    Add files via upload

---
+++ gpt.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+
+st.title("CloneGPT")
+
+if "message" not in st.session_state:
+    st.session_state.message = []
+
+for message in st.session_state.message:
+    with st.chat_message(message['role']):
+        st.write(message['content'])
+
+if prompt := st.chat_input("Ask anything..."):
+    with st.chat_message('user'):
+        st.write(prompt)
+    st.session_state.message.append({'role':'user','content':prompt})
+
+if prompt:
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBZND0qWVfuWg1tR9yzdw9-5-sGqx8hhoQ"
+    data = {
+        'contents':[{'parts':[{'text':prompt}]}]
+    }
+    response = requests.post(url,json=data)
+    response = (response.json()) 
+    response = response['candidates'][0]['content']['parts'][0]['text']
+
+try:
+    with st.chat_message('assistant'):
+        st.write(response)
+    st.session_state.message.append({'role':'assistant','content':response})
+except:
+    pass
+
+
